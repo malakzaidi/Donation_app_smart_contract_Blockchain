1b313f4464faba0a6b2368072486c5833994a858
malakzaidi <malakzaidi815@gmail.com>
2025-05-14 11:33:05 +0100

    Update the contract to a more secured one

--- contracts/tirelire.sol
+++ contracts/tirelire.sol
@@ -2,8 +2,17 @@
 pragma solidity ^0.8.0;
 contract Tirelire {
 uint public balance;
+address private owner;
+constructor() {
+owner = msg.sender;
+}
 receive() external payable {
 balance += msg.value;
-// Syntaxe alternative : balance = balance + msg.value;
+}
+function withdraw(uint amount, address payable destAddr) public {
+require(msg.sender == owner, "just the owner can withdraw");
+require(amount <= balance, "not enough balance for withdrawal");
+destAddr.transfer(amount);
+balance -= amount;
 }
 }
